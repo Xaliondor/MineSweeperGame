993561c9828bd1181c65f4a3a1d0efbff61bb972
Angel R. Rivera <anra.rivera@gmail.com>
2016-10-14 23:18:02 -0400

    Final version

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,7 +1,8 @@
 import javax.swing.JFrame;
+import java.util.Random;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		JFrame myFrame = new JFrame("Color Grid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
@@ -9,10 +10,9 @@ public class Main {
 
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
-
+			
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		myFrame.addMouseListener(myMouseAdapter);
-
 		myFrame.setVisible(true);
 	}
 }
--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -3,12 +3,95 @@ import java.awt.Component;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.Random;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MyMouseAdapter extends MouseAdapter {
+	public static int grayRect;
+
+	public void nrbyBombs(MyPanel myPanel, int x, int y){
+		int counter = 0;
+		if((x -1 >= 0 && x -1 < 9) &&  (y-1 >= 0 && y -1 < 9) && myPanel.locOfBomb(x -1, y -1 )){
+			counter ++;
+		} 
+		if((x -1 >= 0 && x -1 < 9) &&  (y >= 0 && y < 9) && myPanel.locOfBomb(x -1, y)){
+			counter ++;
+		} 
+		if((x -1 >= 0 && x -1 < 9) &&  (y + 1 >= 0 && y + 1 < 9) && myPanel.locOfBomb(x -1, y + 1)){
+			counter ++;
+		} 
+		if((x  >= 0 && x < 9) &&  (y-1 >= 0 && y-1 < 9) && myPanel.locOfBomb(x, y-1)){
+			counter ++;
+		} 
+		if((x >= 0 && x < 9) &&  (y + 1>= 0 && y + 1< 9) && myPanel.locOfBomb(x, y + 1)){
+			counter ++;
+		} 
+		if((x + 1 >= 0 && x + 1 < 9) &&  (y - 1 >= 0 && y - 1 < 9) && myPanel.locOfBomb(x + 1, y - 1)){
+			counter ++;
+		} 
+		if((x +1 >= 0 && x + 1< 9) &&  (y >= 0 && y < 9) && myPanel.locOfBomb(x + 1, y)){
+			counter ++;
+		} 
+		if((x + 1 >= 0 && x + 1 < 9) &&  (y + 1>= 0 && y + 1 < 9) && myPanel.locOfBomb(x + 1, y + 1)){
+			counter ++;
+		} 
+
+		//Set the number for near by bombs
+		if (counter > 0) {
+			Color newColor = Color.LIGHT_GRAY;
+			myPanel.colorArray[x][y] = newColor;	
+			myPanel.numForNrbyBombs[x][y] = counter;
+
+		} else {
+			//If no bombs are found
+			if((x - 1 >= 0 && x - 1 < 9) &&  (y >= 0 && y < 9) && !myPanel.colorArray[x - 1][y].equals(Color.GRAY) && !myPanel.colorArray[x - 1][y].equals(Color.RED) && !myPanel.locOfBomb(x - 1, y)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x - 1][y] = newColor;
+				nrbyBombs(myPanel, x - 1, y);	
+			} 
+			if((x - 1 >= 0 && x - 1 < 9) &&  (y -1 >= 0 && y -1 < 9) && !myPanel.colorArray[x - 1][y -1].equals(Color.GRAY) && !myPanel.colorArray[x - 1][y -1].equals(Color.RED) && !myPanel.locOfBomb(x - 1, y -1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x - 1][y -1] = newColor;
+				nrbyBombs(myPanel, x - 1, y -1);
+			} 
+			if((x - 1 >= 0 && x - 1 < 9) &&  (y + 1 >= 0 && y + 1 < 9) && !myPanel.colorArray[x - 1][y + 1].equals(Color.GRAY) && !myPanel.colorArray[x - 1][y + 1].equals(Color.RED) && !myPanel.locOfBomb(x - 1, y + 1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x - 1][y + 1] = newColor;
+				nrbyBombs(myPanel, x - 1, y + 1);
+			} 
+			if((x >= 0 && x < 9) &&  (y -1 >= 0 && y -1 < 9) && !myPanel.colorArray[x][y -1].equals(Color.GRAY) && !myPanel.colorArray[x][y -1].equals(Color.RED) && !myPanel.locOfBomb(x, y -1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x][y -1] = newColor;
+				nrbyBombs(myPanel, x, y -1);
+			} 
+			if((x >= 0 && x < 9) &&  (y + 1 >= 0 && y + 1 < 9) && !myPanel.colorArray[x][y + 1].equals(Color.GRAY) && !myPanel.colorArray[x][y + 1].equals(Color.RED) && !myPanel.locOfBomb(x, y + 1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x][y + 1] = newColor;
+				nrbyBombs(myPanel, x, y + 1);
+			} 
+			if((x + 1 >= 0 && x + 1 < 9) &&  (y >= 0 && y < 9) && !myPanel.colorArray[x + 1][y].equals(Color.GRAY) && !myPanel.colorArray[x + 1][y].equals(Color.RED) && !myPanel.locOfBomb(x + 1, y)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x + 1][y] = newColor;
+				nrbyBombs(myPanel, x + 1, y);
+			} 
+			if((x + 1 >= 0 && x + 1 < 9) &&  (y - 1>= 0 && y -1 < 9) && !myPanel.colorArray[x + 1][y -1].equals(Color.GRAY) && !myPanel.colorArray[x + 1][y - 1].equals(Color.RED) && !myPanel.locOfBomb(x + 1, y - 1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x + 1][y - 1] = newColor;
+				nrbyBombs(myPanel, x + 1, y - 1);
+			} 
+			if((x + 1 >= 0 && x + 1 < 9)&&  (y + 1 >= 0 && y + 1 < 9) && !myPanel.colorArray[x + 1][y + 1].equals(Color.GRAY) && !myPanel.colorArray[x + 1][y + 1].equals(Color.RED) && !myPanel.locOfBomb(x + 1, y + 1)){
+				Color newColor =  Color.GRAY;
+				myPanel.colorArray[x + 1][y + 1] = newColor;
+				nrbyBombs(myPanel, x + 1, y + 1);
+			} 
+		}
+	}
 	private Random generator = new Random();
+	MyPanel myPanel = new MyPanel();
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -31,7 +114,6 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.y = y;
 			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
 			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
-			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
 			//Do nothing
@@ -76,38 +158,87 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Do nothing
 					} else {
 						//Released the mouse button on the same cell where it was pressed
-						if ((gridX == 0) || (gridY == 0)) {
-							//On the left column and on the top row... do nothing
-						} else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
-							switch (generator.nextInt(5)) {
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
+						if ((gridX >= 0) || (gridY >= 0)) {
+							//On the grid:
+							Color newColor = Color.BLACK;
+							if(myPanel.locOfBomb(myPanel.mouseDownGridX, myPanel.mouseDownGridY)){
+								//The losing message
+								JOptionPane.showMessageDialog(myPanel, "You pressed a bomb!", "Game Over", JOptionPane.PLAIN_MESSAGE);
+								myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+								myPanel.repaint();
+
+
 							}
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-							myPanel.repaint();
 						}
+						// Paints empty tiles gray
+						Color newColor = Color.GRAY;
+						myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+						nrbyBombs(myPanel, myPanel.mouseDownGridX, myPanel.mouseDownGridY);
+						myPanel.repaint();
+						grayRect++;
 					}
 				}
 			}
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			//Do nothing
+			Component d = e.getComponent();
+			while (!(d instanceof JFrame)) {
+				d = d.getParent();
+				if (d == null) {
+					return;
+				}
+			}
+			JFrame myFrameRightClick = (JFrame)d;
+			MyPanel myPanelRightClick = (MyPanel) myFrameRightClick.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+			Insets myInsetsRightClick = myFrameRightClick.getInsets();
+			int x1RightClick = myInsetsRightClick.left;
+			int y1RightClick = myInsetsRightClick.top;
+			e.translatePoint(-x1RightClick, -y1RightClick);
+			int xRightClick = e.getX();
+			int yRightClick = e.getY();
+			myPanelRightClick.x = xRightClick;
+			myPanelRightClick.y = yRightClick;
+			myPanelRightClick.mouseDownGridX = myPanelRightClick.getGridX(xRightClick, yRightClick);
+			myPanelRightClick.mouseDownGridY = myPanelRightClick.getGridY(xRightClick, yRightClick);
+			int gridXRightClick = myPanelRightClick.getGridX(xRightClick, yRightClick);
+			int gridYRightClick = myPanelRightClick.getGridY(xRightClick, yRightClick);
+			if ((myPanelRightClick.mouseDownGridX == -1) || (myPanelRightClick.mouseDownGridY == -1)) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((gridXRightClick == -1) || (gridYRightClick == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myPanelRightClick.mouseDownGridX != gridXRightClick) || (myPanelRightClick.mouseDownGridY != gridYRightClick)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					} else {
+						//Released the mouse button on the same cell where it was pressed
+						if ((gridXRightClick >= 0) || (gridYRightClick >= 0)) {
+							//On the grid:
+
+							Color newColor = null;
+							Color newColor2 = null;
+							do{
+								newColor2 = myPanelRightClick.colorArray[myPanelRightClick.mouseDownGridX][myPanelRightClick.mouseDownGridY];
+								switch (generator.nextInt(2)) {
+								case 0:
+									newColor = Color.RED;
+									break;							
+								case 1:
+									newColor = Color.WHITE;
+									break;
+								}
+							}while(newColor.equals(newColor2));
+							myPanelRightClick.colorArray[myPanelRightClick.mouseDownGridX][myPanelRightClick.mouseDownGridY] = newColor;
+							myPanelRightClick.repaint();
+						}
+					}
+				}
+			}
+			myPanelRightClick.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
